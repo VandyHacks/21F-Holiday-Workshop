b32acf18fc245831feb953007bc297c1a1b5e2cf
Kevin Jin <kevjin2002@gmail.com>
2021-11-13 23:03:41 -0600

    bug fixes

--- content.js
+++ content.js
@@ -1,4 +1,24 @@
 document.querySelectorAll('img').forEach(el => {
-    el.style.width = getComputedStyle(el).width;
-    el.src = 'https://upload.wikimedia.org/wikipedia/commons/4/49/Jonathan_G_Meath_portrays_Santa_Claus.jpg';
+    let width = getComputedStyle(el).width;
+    let height = getComputedStyle(el).height;
+    if (width > height) {
+        el.style.width = width;
+        el.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Jonathan_G_Meath_portrays_Santa_Claus.jpg/171px-Jonathan_G_Meath_portrays_Santa_Claus.jpg';
+    } else {
+        el.style.height = height;
+        el.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Jonathan_G_Meath_portrays_Santa_Claus.jpg/171px-Jonathan_G_Meath_portrays_Santa_Claus.jpg';
+    }
 });
+
+const findAndReplace = {
+    "of": "replace",
+    "anchor": "replace",
+		// add more here!
+}
+
+let html = document.querySelector("body").innerHTML;
+for (let find in findAndReplace) {
+    html = html.replaceAll(find, findAndReplace[find]);
+}
+
+document.querySelector("body").innerHTML = html;
