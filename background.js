f0312dc68f0dc7aec67216057ddb59f0983fc583
nkalupahana <avishkank@gmail.com>
2021-11-16 01:55:37 -0600

    Template ready

--- background.js
+++ background.js
@@ -1,14 +1,7 @@
-console.log("Hello, world!")
+console.log("Hello, world!");
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
     if (changeInfo.status === 'complete') {
         chrome.tabs.executeScript(null, {file: "./content.js"}, () => console.log("content.js injected"));
     }
-});
-
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({
-        "replaceImages": true,
-        "santaURL": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Jonathan_G_Meath_portrays_Santa_Claus.jpg/171px-Jonathan_G_Meath_portrays_Santa_Claus.jpg",
-    });
 });
--- content.js
+++ content.js
@@ -1,12 +1,1 @@
-chrome.storage.sync.get(["replaceImages", "santaURL"], options => {
-    if (options.replaceImages) {
-        document.querySelectorAll('img').forEach(el => {
-            const width = getComputedStyle(el).width;
-            const height = getComputedStyle(el).height;
-            el.style.width = width;
-            el.style.height = height;
-            el.src = options.santaURL;
-            el.srcset = "";
-        });
-    }
-});
+// Replace all images with Santa!
