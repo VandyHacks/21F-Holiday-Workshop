30fa3659e071f1582142e5d834a2c492deacf449
nkalupahana <avishkank@gmail.com>
2021-11-16 18:25:01 -0600

    Add santa

--- content.js
+++ content.js
@@ -1,1 +1,2 @@
 // Replace all images with Santa!
+// https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Jonathan_G_Meath_portrays_Santa_Claus.jpg/171px-Jonathan_G_Meath_portrays_Santa_Claus.jpg
